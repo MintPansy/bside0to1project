38daa313d4a80c4142d9bcb1368b9ef694cbd4d9
hjkl <goni1653@hufs.ac.kr>
2026-01-22 17:57:28 +0900

    asdf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Prisma 클라이언트를 번들에 포함
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client')
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
